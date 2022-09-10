27e38a6fb3b5057b0aa95f8d764e54f574db49f3
SahilHakimiUofT <71340008+SahilHakimiUofT@users.noreply.github.com>
2022-09-10 18:44:09 -0400

    working on homepage carousel

--- bookd-front-end/src/pages/Home/HomePage.jsx
+++ bookd-front-end/src/pages/Home/HomePage.jsx
@@ -1,7 +1,22 @@
+import Carousel from "react-bootstrap/Carousel";
+import classes from "./HomePage.module.css";
+import k1 from "../../assets/k1.jpg";
+import bowling from "../../assets/bowling.jpg";
+import escape from "../../assets/escape.jpg";
 const HomePage = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className={classes.pageContainer}>
+      <Carousel key="bob">
+        <Carousel.Item interval={1000} key="steve">
+          <img className="d-block w-100" src={k1} alt="First slide" />
+        </Carousel.Item>
+        <Carousel.Item interval={1000} key="jim">
+          <img className="d-block w-100" src={bowling} alt="Third slide" />
+        </Carousel.Item>
+        <Carousel.Item interval={1000} key="cop">
+          <img className="d-block w-100" src={escape} alt="Third slide" />
+        </Carousel.Item>
+      </Carousel>
     </div>
   );
 };
